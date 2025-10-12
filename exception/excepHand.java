028712f7da003074e4ab2a421c2e5ceb3234053e
vasumawa74 <vasumawa74@gmail.com>
2025-10-12 20:02:05 +0530

    More on Exception handling

--- exception/excepHand.java
+++ exception/excepHand.java
@@ -3,18 +3,19 @@ public class excepHand
     public static void main(String[]args)
     {
         @SuppressWarnings("unused")
-        int a=10,b=5,x,c=5,y;
+        int a=10,b=5,x,c=5,y;   //taking b=c=5 so that b-c=0
 
         try
         {
-        x=a/(b-c);  //exception body
+            x=a/(b-c);  //actual logic -- critical statement
         }
-        catch(ArithmeticException e)
+
+        catch(ArithmeticException e) //only executed in case of exception
         {
-         System.out.println("Divide by zero");
+         System.out.println("\tException : Division by zero"+e);  //exception body
         }
 
         y=a/(b-c);
-        System.out.println("y="+y);
+        System.out.println("Result ="+y);
     }
 }
---
+++ exception/multierr.java
@@ -0,0 +1,45 @@
+//Exception handling of multiple errors
+
+public class multierr 
+{
+   public static void main (String [] args)
+   {
+    int a, b=10;
+
+    int num[]=new int[5];
+     
+    String abc=null;
+    
+    try
+    {
+      a=18/b;
+      System.out.println(abc.length());
+      System.out.println(num[1]);
+      System.out.println(num[4]);
+    }
+
+    catch(ArithmeticException e)
+    {
+       System.out.println("Something went wrong...\n\t Cannot divide by zero");
+    }
+
+    catch(ArrayIndexOutOfBoundsException v)
+    {
+      System.out.println("Something went wrong...\n\tThe Given array index is out of bound ");
+    }
+
+    catch(NullPointerException w)
+    {
+      System.out.println("Something went wrong...\n\tNull String doesnt Have any length");
+    }
+    
+
+    catch(Exception p)
+    {
+      System.out.println("-Default exception-Something went wrong...");
+    }
+    
+
+    System.out.println("Result = ");
+   }    
+}
